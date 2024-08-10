67df037ba3c4ab93c98c9674d9f0c84551660cd1
Elitecrafttech <Elitecrafttech@gmail.com>
2024-08-10 10:27:55 +0100

    space commit

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -7,10 +7,10 @@ const home = () => {
   return (
     <nav className='size-full flex h-[70px] items-center
     justify-between md:justify-normal px-[30px] md:px-[60px]
-    md:gap-[200px]'>
+    md:gap-[100px] lg:gap-[200px]'>
         <img src={image} alt="" />
-        <div className='hidden md:flex gap-[200px]'>
-        <ul className='flex gap-[40px] font-semibold'>
+        <div className='hidden md:flex md:gap-[100px] lg:gap-[200px]'>
+        <ul className='flex md:gap-[15px] lg:gap-[40px] font-semibold'>
             <li>Pricing</li>
             <li>Product</li>
             <li>About Us</li>
